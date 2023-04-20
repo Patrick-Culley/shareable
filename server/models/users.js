b0cafb111429ecdf6d1b6f50e4afbf134452f6d1
patbc <culleyp@oregonstate.edu>
2023-04-20 18:06:59 -0500

    file setup

---
+++ server/models/users.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+    {
+        firstName: {
+            type: String,
+            required: true, 
+            min: 2, 
+            max: 50,
+        },
+        lastName: {
+            type: String,
+            required: true, 
+            min: 2, 
+            max: 50,
+        },
+        email: {
+            type: String,
+            required: true, 
+            unique: true, 
+            max: 50,
+        },
+        password: {
+            type: String,
+            required: true, 
+            min: 2, 
+        },
+        picturePath: {
+            type: String,
+            default: "",
+        },
+        friends: {
+            type: Array,
+            default: [],
+        },
+        location: String, 
+        occupation: String, 
+        ViewedProfile: Number, 
+        impressions: Number,
+    }, {timestamps: true}
+)
+
+const User = mongoose.model("User", UserSchema);
+export default User; 
